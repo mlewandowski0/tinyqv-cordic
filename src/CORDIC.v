626a0c0d9805d71014b1c711aa9c02bb06df0915
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2025-08-20 13:03:15 +0100

    added cordic

---
+++ src/CORDIC.v
@@ -0,0 +1,214 @@
+`include "defines.v"
+
+// Unified microrotation algortihm can be expressed in the following form
+// x[j+1] = x[j] - m * \sigma_{j} 2^{-j} y[j]
+// y[j+1] = y[j] + \sigma_{j} 2^{-j} x[j]
+//
+//           { z[j] - sigma_{j} * tan^{-1} (2^{-j})    if m = 1
+//  z[j+1] = { z[j] - sigma_{j} * tanh^{-1} (2^{-j})   if m = -1
+//           { z[j] - sigma_{j} * tanh
+
+
+
+module CORDIC #(
+    parameter ITERATIONS = 9,
+    parameter FIXED_WIDTH = 16
+) (
+    input                           clk,
+    input                           rst_n,
+    input                           start,
+    input                           is_rotating,            // 1 = rotate, 0 = vector
+    input [1:0]                     mode,                   // `CIRCULAR_MODE`, `LINEAR_MODE`, `HYPERBOLIC_MODE`
+    input [$clog2(FIXED_WIDTH)-1:0] alpha_one_left_shift,   // on which bit, the 1.0 is stored 
+                                                            // for example for WIDTH=16 and this value set to 10
+                                                            // 1.0 = 0000 0100 0000 0000
+
+    input [FIXED_WIDTH-1:0]         A,                      // first input to module
+    input [FIXED_WIDTH-1:0]         B,                      // second input to module
+    output reg [FIXED_WIDTH-1:0]    out1,                   // first ouput
+    output reg [FIXED_WIDTH-1:0]    out2,                   // second output
+    output reg done                                         // 1-cycle pulse on finish, pluggable to interrupt ? 
+);
+
+    localparam ITERATION_WIDTH = $clog2(ITERATIONS)-1;
+
+    // internal path
+    reg signed [FIXED_WIDTH-1:0] x;
+    reg signed [FIXED_WIDTH-1:0] y;
+    reg signed [FIXED_WIDTH-1:0] z;
+
+    wire signed [FIXED_WIDTH-1:0] next_x;
+    wire signed [FIXED_WIDTH-1:0] next_y;
+    wire signed [FIXED_WIDTH-1:0] next_z;
+
+    // iteration counter
+    reg [ITERATION_WIDTH:0] iteration;
+
+    // shift amount clamped
+    wire [ITERATION_WIDTH:0] sh = (iteration > (FIXED_WIDTH-1)) ? (FIXED_WIDTH-1) : iteration;
+
+    // determining the sigma sign
+    wire is_sigma_positive = (is_rotating) ? (z >= 0) : (y < 0); // rotate : sign(z); vector : -sign(y)
+
+    wire signed [FIXED_WIDTH-1:0] alpha_one_value = 1 << alpha_one_left_shift;
+
+    // needs updating for smaller iterations
+    localparam signed [FIXED_WIDTH-1:0] K_INV_Q = 16'sd9949; // ≈0.607254
+
+    // atan lookup table
+    wire signed [FIXED_WIDTH-1:0] delta_theta;
+    CORDIC_angles_ROM #(.FIXED_WIDTH(FIXED_WIDTH),
+                               .ITERATIONS(ITERATIONS)) angles_rom(
+        .clk(clk),
+        .which_angle(sh),
+        .angle_out(delta_theta)
+    );
+
+
+    wire signed [FIXED_WIDTH-1:0] delta_z;
+    assign delta_z = (mode == `CIRCULAR_MODE) ? delta_theta : 
+                  (mode == `LINEAR_MODE) ? (alpha_one_value >> (iteration-1)) :
+                  (mode == `HYPERBOLIC_MODE) ? 1 : 0;
+
+    CORDIC_iteration #(.FIXED_WIDTH(FIXED_WIDTH),
+                       .ITERATIONS(ITERATIONS)) iter( .x(x),
+                                                      .y(y),
+                                                      .z(z),
+                                                      .shift(sh-1),
+                                                      .delta_z(delta_z),
+
+                                                      .is_sigma_positive(is_sigma_positive),
+                                                      .mode(mode),
+
+                                                      .next_x(next_x),
+                                                      .next_y(next_y),
+                                                      .next_z(next_z));
+
+    // running flag
+    reg running;
+
+    // 1-cycle done pulse
+    wire last_iter = (iteration == (ITERATIONS-1));
+
+    always @(posedge clk) begin
+        if (!rst_n) 
+        begin
+            running <= 1'b0;
+            iteration <= 8'd0;
+            x <= '0;
+            y <= '0;
+            z <= '0;
+            out1 <= '0;
+            out2 <= '0;
+            done <= 1'b0;
+        end 
+        else 
+        begin
+            
+            // default 
+            done <= 1'b0;
+
+            if (start && !running)
+            begin 
+                running <= 1'b1;
+                iteration <= 8'd1;
+
+                // initialize per mode + rotation/vector mode
+
+                case(mode)
+                    `CIRCULAR_MODE:
+                    begin
+                        if (is_rotating)
+                        begin
+                            // rotate by angle A : output cos/sin in x/y
+                            z <= A;
+                            x <= K_INV_Q; // 16'b0100_0000_0000_0000; //
+                            y <= {FIXED_WIDTH{1'b0}};
+                        end
+                        else 
+                        begin
+                            // vectoring mode : input A,B output = K * sqrt(A^2 + B^2), z=atan(B/A)
+                            x <= $signed(A);
+                            y <= $signed(B);
+                            z <= 0;
+                        end
+                    end
+                    `LINEAR_MODE:
+                    begin
+                        
+                        if (is_rotating)
+                        begin
+                            // multiplication : input A, B, output  ~ A * B (output 2 stores something like and error, kind of)
+                            x <= $signed(A);
+                            y <= 0;
+                            z <= $signed(B);
+                        end
+                        else 
+                        begin
+                            // division : input A, B, output  ~ A / B (output 2 stores something like and error, kind of)
+                            x <= $signed(A);
+                            y <= $signed(B);
+                            z <= 0;
+                        end
+                    end
+                    `HYPERBOLIC_MODE:
+                    begin
+                    end
+
+                    default:
+                    begin
+                        x <= 0;
+                        y <= 0;
+                        z <= 0;
+                    end
+                endcase
+            end
+            else if (running)
+            begin
+                // perform iteration
+                x <= next_x;
+                y <= next_y;
+                z <= next_z;
+
+            if (last_iter) 
+            begin
+                    running <= 1'b0;
+                    done    <= 1'b1;
+                    iteration <= 1'b0;
+                    case (mode)
+                        `CIRCULAR_MODE: begin
+                            if (is_rotating) begin
+                                out1 <= x; // cos
+                                out2 <= y; // sin
+                            end else begin
+                                out1 <= x; // r ≈ K*sqrt(A^2+B^2)
+                                out2 <= z; // atan2(B,A)
+                            end
+                        end
+                        `LINEAR_MODE: begin
+                            if (is_rotating)
+                            begin
+                                out1 <= y;  // A * B 
+                                out2 <= z;  // ~ error
+                            end
+                            else 
+                            begin
+                                out1 <= z;  // B / A
+                                out2 <= y;  // ~ error
+                            end
+                        end
+                        default: begin
+                            out1 <= '0; out2 <= '0;
+                        end
+                    endcase
+                end 
+                else 
+                begin
+                    iteration <= iteration + 1'b1;
+                end
+            end
+        end
+    end
+
+
+endmodule
--- test/test.py
+++ test/test.py
@@ -7,6 +7,17 @@ from cocotb.triggers import ClockCycles
 
 from tqv import TinyQV
 
+
+
+# BITS for mode
+MODE_BITS           = 2
+CIRCULAR_MODE       = 0
+LINEAR_MODE         = 1
+HYPERBOLIC_MODE     = 2
+
+# 
+IS_ROTATING_BIT     = 1 
+
 # When submitting your design, change this to the peripheral number
 # in peripherals.v.  e.g. if your design is i_user_peri05, set this to 5.
 # The peripheral number is not used by the test harness.
@@ -33,45 +44,70 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Test register write and read back
-    await tqv.write_word_reg(0, 0x82345678)
-    assert await tqv.read_byte_reg(0) == 0x78
-    assert await tqv.read_hword_reg(0) == 0x5678
-    assert await tqv.read_word_reg(0) == 0x82345678
+    value = await tqv.read_word_reg(0) 
 
-    # Set an input value, in the example this will be added to the register value
-    dut.ui_in.value = 30
+    # read the identificator
+    assert value == 0xbadcaffe, "when reading from reg 0, we should see magic string '0xbadcaffe'"
 
-    # Wait for two clock cycles to see the output values, because ui_in is synchronized over two clocks,
-    # and a further clock is required for the output to propagate.
+    # Check the status register : we don't yet run anything, it should be 0
+    assert await tqv.read_byte_reg(6) == 0, "status register should be 0 (READY TO BE RUN)"
+ 
+    # outputs should be zeroed out 
+    assert await tqv.read_hword_reg(4) == 0, "output 1 should be zeroed out"
+    assert await tqv.read_hword_reg(5) == 0, "output 2 should be zeroed out"
+
+    # first test : compute the sin(30 degrees) and cos(30 degrees)
+
+    # the input to circular mode in rotating, is only angle, stored as radians, fixed point 
+    # arithmetic in signed 1.14 bits format ( for FIXED WIDTH = 16, in general case, in signed 1.{FIXED_WIDTH-2} format )
+    # 30 degrees = pi / 6 = 0.52359877 \approx (in fixed point) b00100001_10000011
+    await tqv.write_word_reg(1, 0b0)
+ 
+    # configure the cordic : set the mode to CIRCULAR, ROTATING, and running 
+    # this corresponds to setting it to       {2'b00,   1'b1,         1'b1 }
+    
+    config_to_write = (CIRCULAR_MODE << MODE_BITS) | (1 << IS_ROTATING_BIT) | 1     
+    dut._log.info(f"Configuring CORDIC with {config_to_write:#04x} (mode={CIRCULAR_MODE}, is_rotating=1, start=1)")
+    await tqv.write_byte_reg(0, config_to_write)
+   
+    # check if the device is busy
     await ClockCycles(dut.clk, 3)
+    #assert await tqv.read_byte_reg(6) == 1, "status register should be 1 (BUSY)"
+
+    await ClockCycles(dut.clk, 10)
+    #assert await tqv.read_byte_reg(6) == 2, "status register should be 2 (DONE)"
+
+    out1 = await tqv.read_hword_reg(4)
+    out2 = await tqv.read_hword_reg(5)
+    dut._log.info(f"out1 = {out1}, out2 = {out2}")
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0x96
+    # assert dut.uo_out.value == 0x96
 
     # Input value should be read back from register 1
-    assert await tqv.read_byte_reg(4) == 30
+    #assert await tqv.read_byte_reg(4) == 30
 
     # Zero should be read back from register 2
-    assert await tqv.read_word_reg(8) == 0
+    #assert await tqv.read_word_reg(8) == 0
 
     # A second write should work
-    await tqv.write_word_reg(0, 40)
-    assert dut.uo_out.value == 70
+    #await tqv.write_word_reg(0, 40)
+    #assert dut.uo_out.value == 70
 
     # Test the interrupt, generated when ui_in[6] goes high
-    dut.ui_in[6].value = 1
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in[6].value = 0
+    #dut.ui_in[6].value = 1
+    #3await ClockCycles(dut.clk, 1)
+    #dut.ui_in[6].value = 0
 
     # Interrupt asserted
-    await ClockCycles(dut.clk, 3)
-    assert await tqv.is_interrupt_asserted()
+    #await ClockCycles(dut.clk, 3)
+    #assert await tqv.is_interrupt_asserted()
 
     # Interrupt doesn't clear
-    await ClockCycles(dut.clk, 10)
-    assert await tqv.is_interrupt_asserted()
+    #await ClockCycles(dut.clk, 10)
+    #assert await tqv.is_interrupt_asserted()
     
     # Write bottom bit of address 8 high to clear
-    await tqv.write_byte_reg(8, 1)
-    assert not await tqv.is_interrupt_asserted()
+    #await tqv.write_byte_reg(8, 1)
+    #assert not await tqv.is_interrupt_asserted()
